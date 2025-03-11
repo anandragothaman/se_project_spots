94f217355212514c71fb14582b9c1f1323432221
Anand Ragothaman <r.anandragothaman@gmail.com>
2025-03-11 01:29:07 -0500

    Add new post, like and delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialCards = [
+let initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -29,20 +29,29 @@ const nameInput = document.querySelector("#name");
 const jobInput = document.querySelector("#description");
 const profileNameElement = document.querySelector(".profile__name");
 const profileJobElement = document.querySelector(".profile__description");
-const modalContainer = document.querySelector("#edit-modal");
+const editModal = document.querySelector("#edit-modal");
 const cardsList = document.querySelector(".cards__list");
+const postFormElement = document.forms["new-post"];
+const postModal = document.querySelector("#post-modal");
+const imageLinkInput = document.querySelector("#imageLink");
+const captionInput = document.querySelector("#caption");
 
-function toggleEditModal() {
-  nameInput.value = profileNameElement.textContent;
-  jobInput.value = profileJobElement.textContent;
-  modalContainer.classList.toggle("modal_opened");
+function toggleModal(modal) {
+  if (modal.id === "edit-modal") {
+    nameInput.value = profileNameElement.textContent;
+    jobInput.value = profileJobElement.textContent;
+  } else {
+    captionInput.value = "";
+    imageLinkInput.value = "";
+  }
+  modal.classList.toggle("modal_opened");
 }
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = nameInput.value;
   profileJobElement.textContent = jobInput.value;
-  toggleEditModal();
+  toggleModal(editModal);
 }
 
 function getCardElement(data) {
@@ -50,20 +59,57 @@ function getCardElement(data) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
+
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("card__like-button_liked");
+  });
+
+  cardDeleteButton.addEventListener("click", () => {
+    cardDeleteButton.closest(".card").remove();
+  });
   return cardElement;
 }
-const editProfileButton = document.querySelector(".profile__edit-btn");
-editProfileButton.addEventListener("click", toggleEditModal);
 
-const closeModalButton = document.querySelector(".modal__close-btn");
-closeModalButton.addEventListener("click", toggleEditModal);
+function handlePostFormSubmit(evt) {
+  evt.preventDefault();
+  const newCard = { name: String, link: String };
+  newCard.name = captionInput.value;
+  newCard.link = imageLinkInput.value;
+  const newCardElement = getCardElement(newCard);
+  cardsList.prepend(newCardElement);
+  toggleModal(postModal);
+}
+
+const editProfileButton = document.querySelector(".profile__edit-btn");
+editProfileButton.addEventListener("click", () => {
+  toggleModal(editModal);
+});
+
+const closeModalButton = document.querySelector(".modal__close-edit-btn");
+closeModalButton.addEventListener("click", () => {
+  toggleModal(editModal);
+});
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = getCardElement(initialCards[i]);
+initialCards.forEach((initialCard) => {
+  const cardElement = getCardElement(initialCard);
   cardsList.append(cardElement);
-}
+});
+
+const addProfileButton = document.querySelector(".profile__add-btn");
+addProfileButton.addEventListener("click", () => {
+  toggleModal(postModal);
+});
+
+const closeEditModalButton = document.querySelector(".modal__close-post-btn");
+closeEditModalButton.addEventListener("click", () => {
+  toggleModal(postModal);
+});
+
+postFormElement.addEventListener("submit", handlePostFormSubmit);
