26ad0a51cbf4eed587964d08fdcd1f219987849c
Anand Ragothaman <r.anandragothaman@gmail.com>
2025-03-12 23:58:51 -0500

    Add preview modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 let initialCards = [
   {
+    name: "Golden gate bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -33,6 +37,7 @@ const editModal = document.querySelector("#edit-modal");
 const cardsList = document.querySelector(".cards__list");
 const postFormElement = document.forms["new-post"];
 const postModal = document.querySelector("#post-modal");
+const previewModal = document.querySelector("#preview-modal");
 const imageLinkInput = document.querySelector("#imageLink");
 const captionInput = document.querySelector("#caption");
 
@@ -40,7 +45,7 @@ function toggleModal(modal) {
   if (modal.id === "edit-modal") {
     nameInput.value = profileNameElement.textContent;
     jobInput.value = profileJobElement.textContent;
-  } else {
+  } else if (modal.id === "post-modal") {
     captionInput.value = "";
     imageLinkInput.value = "";
   }
@@ -61,6 +66,8 @@ function getCardElement(data) {
   const cardTitle = cardElement.querySelector(".card__title");
   const cardLikeButton = cardElement.querySelector(".card__like-button");
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  const previewImage = previewModal.querySelector(".modal__image");
+  const previewTitle = previewModal.querySelector(".modal__caption");
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
@@ -71,6 +78,13 @@ function getCardElement(data) {
 
   cardDeleteButton.addEventListener("click", () => {
     cardDeleteButton.closest(".card").remove();
+  });
+
+  cardImage.addEventListener("click", () => {
+    previewImage.src = cardImage.src;
+    previewImage.alt = cardImage.alt;
+    previewTitle.textContent = data.name;
+    toggleModal(previewModal);
   });
   return cardElement;
 }
@@ -110,6 +124,13 @@ addProfileButton.addEventListener("click", () => {
 const closeEditModalButton = document.querySelector(".modal__close-post-btn");
 closeEditModalButton.addEventListener("click", () => {
   toggleModal(postModal);
+});
+
+const closePreviewModalButton = document.querySelector(
+  ".modal__close-type-preview"
+);
+closePreviewModalButton.addEventListener("click", () => {
+  toggleModal(previewModal);
 });
 
 postFormElement.addEventListener("submit", handlePostFormSubmit);
